02b229bc016465d05511f492b5a6ca4065dcb449
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-04-11 21:30:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add qosmio nss-package
 # echo 'src-git nss_packages https://github.com/qosmio/nss-packages.git;NSS-12.4-K6.x' >>feeds.conf.default
-echo 'src-git nss_packages https://github.com/AgustinLorenzo/nss-packages.git;main_nss_wifi' >>feeds.conf.default
+# echo 'src-git nss_packages https://github.com/AgustinLorenzo/nss-packages.git;main_nss_wifi' >>feeds.conf.default
